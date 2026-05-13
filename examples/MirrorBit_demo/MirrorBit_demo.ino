0a226c7e40e131208a7728caad2c406d9809e0a1
Jack Pyburn <jwpyburn05@gmail.com>
2026-05-13 14:16:14 -0500

    Update MirrorBit_demo example sketch

--- examples/MirrorBit_demo/MirrorBit_demo.ino
+++ examples/MirrorBit_demo/MirrorBit_demo.ino
@@ -1,19 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+// Filename: MirrorBit_demo.ino
+// Author: Jack Pyburn
+// Description: This is an example file for demonstrating basic features of the
+// S29GL064S class. 
+////////////////////////////////////////////////////////////////////////////////
 #include <S29GL064S.h>
 #include <vector>
 #include <stdint.h>
 
-#define KWORD 1024
+static const std::vector<int> address_DUE_pins = {
+  22, 23, 24, 25, 26, 27, 28, 29,
+  30, 31, 32, 33, 34, 35, 36, 37,
+  38, 39, 40, 41, 42, 43
+};
 
-#define ANSI_COLOR_DEFAULT "\x1B[0m"
-#define ANSI_COLOR_GREEN "\x1B[32m"
+static const std::vector<int> data_DUE_pins = {
+  54, 55, 56, 57, 58, 59, 60, 61,
+  62, 63, 64, 65, 66, 67, 68, 69
+};
 
-// Pin setup for IC
-S29GL064S memory(
-  {22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
-    36, 37, 38, 39, 40, 41, 42, 43},
-  {54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69},
-  48, 49, 50, 8, 51, 7
-); 
+static const int WE_pin = 48;
+static const int OE_pin = 49;
+static const int CE_pin = 50;
+static const int RYBY_pin = 8; // This isn't used on the V6 version of the chip
+static const int RST_pin = 51;
+static const int WP_pin = 7;
+
+static S29GL064S memory(address_DUE_pins, data_DUE_pins, 
+                        WE_pin, OE_pin, CE_pin, RYBY_pin, RST_pin, WP_pin);
 
 void setup() 
 {
@@ -23,7 +37,10 @@ void setup()
   memory.begin();
 
   while (!SerialUSB);
-  while ((char)SerialUSB.read() != '\r');
+  while ((char)SerialUSB.read() != '\r'); // wait for user to press enter
+
+  memory.chip_erase();
+  memory.word_program(0x0064, 0xAABB);
 
   for (uint32_t i = 0; i < 4194304; ++i) {
     uint32_t temp = i;
@@ -41,15 +58,4 @@ void setup()
 
 }
 
-void loop() {
-  char buffer[45];
-
-  while ((char)Serial.read() != '\r');
-
-  for (int i = 0; i < KWORD; ++i) {
-    sprintf(buffer, "%04X ", memory.word_read(0x0000 + i));
-    Serial.print(buffer);
-    if (!(i % 16))
-      Serial.println();
-  }
-}
+void loop() {}
